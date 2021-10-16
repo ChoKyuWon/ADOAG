2fbe5586ec4ef1bda36e978d13c1ac9c7b1daef5
ChoKyuWon <kyuwoncho18@gmail.com>
2021-10-17 06:02:53 +0900

    iCFG: modularizing the code
    Signed-off-by: ChoKyuWon <kyuwoncho18@gmail.com>

--- iCFG/main.py
+++ iCFG/main.py
@@ -1,29 +1,13 @@
 import sys
+import os
 from capstone import *
 from capstone.x86 import *
 
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
-def main():
-    binary_path = "../examples/main.o"
-    if len(sys.argv) > 1:
-        binary_path = sys.argv[1]
-    bin = open(binary_path, 'rb')
-    elf = ELFFile(bin)
-    section = elf.get_section_by_name('.text')
-    if not section:
-        print("[X] Can't find text section in binary!")
-        exit()
-    offset = elf._get_section_header(elf._section_name_map.get('.text', None))['sh_offset']
-    disassembler = Cs(CS_ARCH_X86, CS_MODE_64)
-    disassembler.detail = True
-    _assembly = disassembler.disasm(section.data(), offset)
-    assembly = []
-    indirects = []
-    for i in _assembly:
-        assembly.append(i)
-
+def indirect_list(assembly, disassembler, section, offset):
+    indirects= []
     for index, insn in enumerate(assembly):
         if insn.mnemonic == 'ud2':
             cmp = None
@@ -58,12 +42,36 @@ def main():
                     break
                 jump_table_targets.append(k.op_find(X86_OP_IMM, 1).imm)
             indirects.append([call.address, jump_table_targets])
-    
-    for i in indirects:
+    return indirects
+
+def print_list(_list):
+    for i in _list:
         print(hex(i[0]), '-> ', end="")
         for j in i[1]:
             print(hex(j), end=", ")
         print()
+def main():
+
+    binary_path = os.path.dirname(os.path.abspath(__file__)) + "/../examples/main.o"
+    if len(sys.argv) > 1:
+        binary_path = sys.argv[1]
+    bin = open(binary_path, 'rb')
+    elf = ELFFile(bin)
+    section = elf.get_section_by_name('.text')
+    if not section:
+        print("[X] Can't find text section in binary!")
+        exit()
+    offset = elf._get_section_header(elf._section_name_map.get('.text', None))['sh_offset']
+    disassembler = Cs(CS_ARCH_X86, CS_MODE_64)
+    disassembler.detail = True
+    _assembly = disassembler.disasm(section.data(), offset)
+    assembly = []
+    for i in _assembly:
+        assembly.append(i)
+
+    indirects = indirect_list(assembly, disassembler, section, offset)
+    print_list(indirects)
+    
 
 if __name__ == "__main__":
     main()
