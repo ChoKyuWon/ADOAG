b6a7f1304af87ce43add2552a2556947933c0014
ChoKyuWon <kyuwoncho18@gmail.com>
2021-12-01 01:35:20 +0900

    combine iCFG and symbolic at src/ folder
    Signed-off-by: ChoKyuWon <kyuwoncho18@gmail.com>

--- src/main.py
+++ src/main.py
@@ -4,7 +4,15 @@ import subprocess
 import avatar2
 import sys
 import os
+import networkx
 from angr_targets import AvatarGDBConcreteTarget
+
+if __name__ == "__main__" and __package__ is None:
+    from sys import path
+    from os.path import dirname as dir
+
+    path.append(dir(path[0]))
+    __package__ = "iCFG"
 
 from ..iCFG.find_indirects import Indirects
 from ..iCFG.jump_resolver import IFCCReslover
@@ -12,7 +20,8 @@ from ..iCFG.jump_resolver import IFCCReslover
 STDIN_FD = 0
 GDB_SERVER_IP = "localhost"
 GDB_SERVER_PORT = 12345
-TARGET_BINARY = "./main.o"
+binary_path = os.path.dirname(os.path.abspath(__file__)) + "/../examples/main.o"
+TARGET_BINARY = binary_path
 
 base_addr = 0x400000 # To match addresses to Ghidra
 
@@ -23,7 +32,6 @@ subprocess.Popen("gdbserver %s:%s %s" % (GDB_SERVER_IP,GDB_SERVER_PORT,TARGET_BI
 avatar_gdb = AvatarGDBConcreteTarget(avatar2.archs.x86.X86_64, GDB_SERVER_IP, GDB_SERVER_PORT)
 proj = angr.Project(TARGET_BINARY, main_opts={'base_addr': base_addr}, concrete_target=avatar_gdb, use_sim_procedures=True) 
 
-binary_path = os.path.dirname(os.path.abspath(__file__)) + "/../examples/main.o"
 if len(sys.argv) > 1:
     binary_path = sys.argv[1]
 bin = open(binary_path, 'rb')
@@ -32,24 +40,23 @@ indirects = Indirects(bin)
 indirects.indirect_list()
 
 indirect_reslover = IFCCReslover(proj, indirects.indirects)
-# cfg = proj.analyses.CFGFast(
-#     function_starts=[main.rebased_addr], 
-#     indirect_jump_resolvers = tuple(
-# 	angr.analyses.cfg.indirect_jump_resolvers.default_resolvers.default_indirect_jump_resolvers(
-# 		proj.loader.main_object,
-# 		proj
-# 	)) + (indirect_reslover,)
-# )
-cfg = proj.analyses.CFGEmulated(
-    keep_state=True,
-    state_add_options=angr.sim_options.refs, 
-    context_sensitivity_level=2,
+
+main = proj.loader.main_object.get_symbol("main")
+vuln = proj.loader.main_object.get_symbol("vuln")
+target = proj.loader.main_object.get_symbol("target")
+
+
+proj_cfg = angr.Project(binary_path, load_options={'auto_load_libs':False})
+cfg = proj_cfg.analyses.CFGFast(
+    function_starts=[main.rebased_addr], 
     indirect_jump_resolvers = tuple(
-    angr.analyses.cfg.indirect_jump_resolvers.default_resolvers.default_indirect_jump_resolvers(
-        proj.loader.main_object,
-        proj
-    )) + (indirect_reslover,)
+	angr.analyses.cfg.indirect_jump_resolvers.default_resolvers.default_indirect_jump_resolvers(
+		proj_cfg.loader.main_object,
+		proj_cfg
+	)) + (indirect_reslover,)
 )
+
+
 
 src_node = cfg.model.get_any_node(vuln.rebased_addr)
 dst_node = cfg.model.get_any_node(target.rebased_addr)
@@ -63,73 +70,16 @@ print("Now we got CFG!")
 paths = networkx.all_simple_paths(cfg.graph, src_node, dst_node)
 
 # paths = networkx.all_simple_paths(cfg.graph, entry_node, vuln_node)
-print(hex(indirects.init_base), hex(indirects.end))
-for path in paths:
-    for node in path:
-        if (node.addr > indirects.init_base) and (node.addr < indirects.end):
-            print(hex(node.addr))
-    print()
+path = []
+for _path in paths:
+    for node in _path:
+        if (node.addr < indirects.init_base) or (node.addr > indirects.end):
+            pass
+        else:
+            path.append(node.addr)
+    break
 # iCFG will give the path to target
-# vuln, first_gate, second_gate, third_gate, target
-path = [
-    0x401310,
-    0x401030,
-    0x40134b,
-    0x401040,
-    0x40137d,
-    0x401399,
-    0x4013c3,
-    0x401240,
-    0x40125e,
-    0x401264,
-    0x40128e,
-    0x401140,
-    0x401159,
-    0x401176,
-    0x4012e0,
-    0x4012f1,
-    0x4011e0
-]
 
-# Not-working example
-# path = [
-# 0x401310,
-# 0x40134b,
-# 0x401360,
-# 0x40137d,
-# 0x401399,
-# 0x4013c3,
-# 0x401240,
-# 0x40125e,
-# 0x401297,
-# 0x4012c5,
-# 0x401190,
-# 0x4011a9,
-# 0x4011c6,
-# 0x4012e0,
-# 0x4012f1,
-# 0x4011e0,
-# ]
-
-# path = [
-# 0x401310,
-# 0x40134b,
-# 0x401360,
-# 0x40137d,
-# 0x401399,
-# 0x4013c3,
-# 0x401240,
-# 0x40125e,
-# 0x401264,
-# 0x40128e,
-# 0x401190,
-# 0x4011a9,
-# 0x4011c6,
-# 0x4012e0,
-# 0x4012f1,
-# 0x4011e0,
-# ]
-vuln = path.pop(0)
 
 # concrete execution
 
@@ -143,7 +93,7 @@ un_init_func_table_addr = 0x404050
 
 simgr = proj.factory.simulation_manager(entry_state)
 
-simgr.use_technique(angr.exploration_techniques.Symbion(find=[vuln]))
+simgr.use_technique(angr.exploration_techniques.Symbion(find=[vuln.rebased_addr]))
 
 exploration = simgr.run()
 vuln_state = None
